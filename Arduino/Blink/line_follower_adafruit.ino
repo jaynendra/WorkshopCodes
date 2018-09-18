791a29932fbf6668eb913dd3e0dfd163d78ef5bb
jaynendra sharma <35022718+jaynendra@users.noreply.github.com>
2018-09-18 20:10:56 +0530

    line follower

---
+++ Arduino/Blink/line_follower_adafruit.ino
@@ -0,0 +1,69 @@
+/*
+ Line follower robot:
+   Line follower is an autonomous robot which follows 
+   either black line in white surface or white line in black surface.
+   Robot must be able to detect particular line and keep following it.
+   HARDWARE REQUIRED:-
+   ARDUINO UNO:-1
+   POWER CABLE:-1
+   ADAFRUIT MOTOR DRIVER SHIELD:-1
+   DC GEARED MOTOR:-2
+   IR SENSORS:-2
+   CHASSIS:-1
+   WHEELS:-2
+   CASTER WHEEL:-1
+   JUMPER WIRE:-6
+   BATTERY(18650):-2
+    The circuit:
+    -analog 1 (A1): input pin for sensor1
+    -analog 2 (A2): input pin for sensor2
+    -motors2: output pin for motor1
+    -motor3: output pin for motor2
+    
+   created and developed at 22 Feb 2018
+   by Jaynendra sharma
+   organisation:- Techprolabz The School Of Robotics
+*/
+
+#include <AFMotor.h>
+AF_DCMotor motor1(2);
+AF_DCMotor motor2(3);
+void setup() {
+ Serial.begin(9600);
+ pinMode(A1,INPUT);
+ pinMode(A2,INPUT);
+ motor1.setSpeed(150);   //set your motor speed acording to your motor specifications  
+ motor2.setSpeed(150);
+ motor1.run(RELEASE);
+ motor2.run(RELEASE);
+
+}
+
+void loop() {
+  int sense1=digitalRead(A1);
+  int sense2=digitalRead(A2);
+if ((sense1==1)&&(sense2==1))
+{
+  Serial.print("f");  // forward direction
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+}
+if ((sense1==1)&&(sense2==0)) //left direction
+{
+  Serial.print("l");
+  motor1.run(FORWARD);
+  motor2.run(RELEASE);
+}
+if((sense1==0)&&(sense2==1))  // right direction
+{
+  Serial.print("r");
+  motor1.run(RELEASE);
+  motor2.run(FORWARD);
+}
+if((sense1==0)&&(sense2==0))   //stop
+{
+  Serial.print("s");
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+}
+}
